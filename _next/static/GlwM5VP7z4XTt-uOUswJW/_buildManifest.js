891e2caa771e29509a711d78961fdd51d363499f
ghostoutfit <josephlkremer@gmail.com>
2021-10-10 12:56:04 -0400

    Updates

---
+++ _next/static/GlwM5VP7z4XTt-uOUswJW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-69f7491780052fc612a1.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
